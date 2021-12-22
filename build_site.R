020874990a1264bd5c58d07b518cf749785ade98
jtmiller <jtmiller@umn.edu>
2021-12-22 10:47:03 -0800

    site

--- build_site.R
+++ build_site.R
@@ -6,3 +6,6 @@ setwd("~/Documents/UNH/classes/TAing/GEN711/GEN711_S21/")
 #render your sweet site. 
 rmarkdown::render_site()
 
+### Miller Notes
+## Directory must contain either an "index.Rmd" or "index.md" file.
+## Directory must contain a site configuration file ("_site.yml").
